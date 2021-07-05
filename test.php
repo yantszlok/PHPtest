0ff982f928f0548230c0b1869f7bc9cda7b84570
yantszlok <yantszlok@sterfield.co.jp>
2021-07-05 14:20:47 +0900

    for increment and decrement

--- test.php
+++ test.php
@@ -1,5 +1,20 @@
 <?php
- for($i=1; $i<=10; $i++)
-     echo $i;
+for($i=1; $i<=10; $i++){
+ echo $i;
+}
+for($t=10; $t>=0; $t--){
+ echo $t;
+}
+
+for($q=1; $q<=31; $q++){
+ echo $q;
+  if($q % 7==0){
+ echo "<br>";
+ }
+}
 ?>
+
+
+
+
 
