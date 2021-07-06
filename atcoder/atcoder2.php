28858242c872729313e298125f7f9274f8638f42
yantszlok <yantszlok@sterfield.co.jp>
2021-07-06 19:31:15 +0900

    修正

--- atcoder/atcoder2.php
+++ atcoder/atcoder2.php
@@ -6,7 +6,7 @@ fscanf(STDIN, "%s",$s);
 
  if( $a<1 || $b>5 ){
    echo "No";
- }elseif($ad + 1 != strlen($ab + 1)){
+ }elseif($ab + 1 != strlen($ab + 1)){
    echo "No";
  }elseif(!preg_match("/\A\d{{$a}}[-]\d{{$b}}\z/", $s)){
    echo "No";
