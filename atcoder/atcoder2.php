9f9da56a1ae691448ab273e7d10e19b879bf861d
yantszlok <yantszlok@sterfield.co.jp>
2021-07-06 20:05:08 +0900

    修正

--- atcoder/atcoder2.php
+++ atcoder/atcoder2.php
@@ -4,12 +4,17 @@ fscanf(STDIN, "%s",$s);
 
  $ab=$a+$b;
 
+$c = strlen($ab);
+
+var_dump($c);
+
  if( $a<1 || $b>5 ){
-   echo "No";
- }elseif($ab + 1 != strlen($ab + 1)){
-   echo "No";
+   echo "No1";
+ }elseif($ab + 1 != strlen($s)){
+// }elseif($ad + 1 != strlen($ab + 1)){
+   echo "No2";
  }elseif(!preg_match("/\A\d{{$a}}[-]\d{{$b}}\z/", $s)){
-   echo "No";
+   echo "No3";
  }else{
    echo "Yes";
 }
