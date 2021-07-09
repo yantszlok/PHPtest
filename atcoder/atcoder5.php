b163d82a68d55a846f46944157db22e53a51754c
yantszlok <yantszlok@sterfield.co.jp>
2021-07-09 11:52:24 +0900

    問題５

---
+++ atcoder/atcoder5.php
@@ -0,0 +1,16 @@
+<?php
+
+fscanf(STDIN, "%s", $s);
+
+if (preg_match('/^[A-Z]+$/', $s)) {
+   $count = strlen($s); //文字列の文字数
+   $s_reverse= strrev($s); //文字列を逆順する
+   $A = strpos($s, 'A'); //Aの前の文字列の文字数
+   $Z = strpos($s_reverse, 'Z'); //逆順した文字列のZの前の文字数
+   $ans= $count - $Z - $A;
+
+   echo $ans;
+ }
+
+
+?>
