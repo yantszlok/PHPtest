1e883ae0ab94c3e5a2ffabfb93dc5cf4ce906d1b
yantszlok <yantszlok@sterfield.co.jp>
2021-07-15 11:59:50 +0900

    問題８

---
+++ atcoder/atcoder8.php
@@ -0,0 +1,21 @@
+<?php
+fscanf(STDIN, "%s", $N);
+fscanf(STDIN, "%s", $a);
+
+$ans = 0;
+
+$A = str_split($a);//文字列を配列に
+ 
+for($i = 0; $i < $N; $i++){
+
+if($A[$i] != $A[$i-1]){ //次の配列と異なる時はスライムの数をプラス１。
+ $ans++;
+ }
+}
+echo $ans;
+
+
+?>
+
+
+
