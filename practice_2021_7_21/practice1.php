7a786ee1b2a4aead6ad901c580aff35271d444dc
yantszlok <yantszlok@sterfield.co.jp>
2021-07-21 14:29:43 +0900

    30分練習

--- practice_2021_7_21/practice1.php
+++ practice_2021_7_21/practice1.php
@@ -1,4 +1,15 @@
 <?php
 
+$S = fgets(STDIN);
+$T = fgets(STDIN);
 
-?>
+$s = str_split($S);
+$t = str_split($T);
+
+$ans = 0;
+for($i=0; $i<3; $i++){
+if($s[$i] == $t[$i]){
+ $anst++;
+  }
+}
+echo $ans;
--- practice_2021_7_21/practice2.php
+++ practice_2021_7_21/practice2.php
@@ -1,4 +1,13 @@
 <?php
+fscanf(STDIN, "%s",$X);
+
+$a = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+
+for($i=0 ; $i<25 ; $i++){
+ if($a[$i]==$X){
+  echo $i+1;
+ }
+} 
 
 ?>
 
