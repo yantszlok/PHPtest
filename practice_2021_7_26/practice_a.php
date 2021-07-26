9f6906df1a188545806aac07c7a20c260341dc77
yantszlok <yantszlok@sterfield.co.jp>
2021-07-26 15:52:22 +0900

    30分練習（修正済み）

--- practice_2021_7_26/practice_a.php
+++ practice_2021_7_26/practice_a.php
@@ -2,10 +2,17 @@
 
 fscanf(STDIN, "%d", $N);
 
+$ans_array = [];
+
 for ($i = 0; $i < $N; $i++) {
 fscanf(STDIN, "%d %d", $A[$i], $B[$i]);
-echo $A[$i] * $B[$i].PHP_EOL;
+$ans_array[] = $A[$i] * $B[$i];
 }
 
+foreach($ans_array as $answer){
 
+
+echo $answer.PHP_EOL;
+}
 ?>
+
--- practice_2021_7_26/practice_b.php
+++ practice_2021_7_26/practice_b.php
@@ -1,15 +1,16 @@
 <?php
 
 $N = fgets(STDIN);
-for($i = 1; $i <= 9; $i++){
-for($j = 1; $j <= 9; $j++){
 
-if($N == $i*$j ){
-echo 'Yes';
-exit;
-}else{
+for($i = 1; $i <= 9; $i++){
+  for($j = 1; $j <= 9; $j++){
+
+    if($N == $i*$j ){
+      echo 'Yes';
+      exit;
+
+    }  
+  }
+}
 echo 'No';
-}
-}
-}
 ?>
