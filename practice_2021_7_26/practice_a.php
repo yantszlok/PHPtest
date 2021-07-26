964075cfa1834b8e094d0a28e5aee1a2f4515f97
yantszlok <yantszlok@sterfield.co.jp>
2021-07-26 15:54:54 +0900

    30分練習（修正済み）

--- practice_2021_7_26/practice_a.php
+++ practice_2021_7_26/practice_a.php
@@ -5,14 +5,12 @@ fscanf(STDIN, "%d", $N);
 $ans_array = [];
 
 for ($i = 0; $i < $N; $i++) {
-fscanf(STDIN, "%d %d", $A[$i], $B[$i]);
-$ans_array[] = $A[$i] * $B[$i];
-}
+	fscanf(STDIN, "%d %d", $A[$i], $B[$i]);
+	  $ans_array[] = $A[$i] * $B[$i];
+}	
 
-foreach($ans_array as $answer){
-
-
-echo $answer.PHP_EOL;
-}
+	foreach($ans_array as $answer){
+	  echo $answer.PHP_EOL;
+        }
 ?>
 
